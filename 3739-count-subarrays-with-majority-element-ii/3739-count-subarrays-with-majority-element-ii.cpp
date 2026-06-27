d75290336d54871d67570be589658658e7b0d66c
Saurabh Shivhare <shivharesaurabh2312@gmail.com>
2026-06-28 00:37:21 +0530

    Time: 7 ms (82.06%), Space: 98.8 MB (60%) - LeetHub

--- 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.cpp
+++ 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.cpp
@@ -1,41 +1,36 @@
 class Solution {
 public:
     long long countMajoritySubarrays(vector<int>& nums, int target) {
-        int n = nums.size();
-        long long cnt = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == target) nums[i] = 1;
-            else nums[i] = -1;
-        }
-
-        vector<int> pref(n);
-        pref[0] = nums[0];
-
-        for (int i = 1; i < n; i++) {
-            pref[i] = pref[i - 1] + nums[i];
-        }
-
-        int shift = n;
-        vector<int> freq(2 * n + 1, 0);
-
-        freq[shift] = 1;
-
-        long long valid = 0;
-        int lastSum = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (pref[i] > lastSum) {
-                valid += freq[lastSum + shift];
-            } else {
-                valid -= freq[pref[i] + shift];
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if (nums[i]==target){
+                nums[i]=1;
             }
-
-            cnt += valid;
-            freq[pref[i] + shift]++;
-            lastSum = pref[i];
+            else{ nums[i]=-1;}
         }
+        vector<int> prefix(n);
+        prefix[0]=nums[0];
+        for(int i=1;i<n;i++){
+            prefix[i]=prefix[i-1]+nums[i];
+        }
+        int shift=n;
+        vector<int> freq(2*n+1,0);
+        long long valid=0;
+        int lastsum=0;
+        freq[shift]=1;
+        long long cnt=0;
+        for(int i=0;i<n;i++){
+            if (prefix[i]>lastsum){
+                valid+=freq[lastsum+shift];
+            }else{
+                valid-=freq[prefix[i]+shift];
+            }
+            cnt+=valid;
+            freq[prefix[i]+shift]++;
+            lastsum=prefix[i];
+                    }
+                    return cnt;
 
-        return cnt;
-    }
+
+        }
 };
